8522642711ec654e274d2bf9b61dbaacae564997
mera2002 <SMIS072121@ugb.edu.sv>
2021-09-21 10:44:00 -0600

    clase autobus

---
+++ introduccion_POO/Autobus.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introduccion_POO
+{
+    class Autobus
+    {
+        //definir los miembros de la clase 
+        //propiedades
+        private int _cod;
+        //private string autobusName; //nombre del autobus
+        private int _number; //numero de ruta 
+        private int _amount; //cantidad de asientos disponibles 
+
+        //metodos de propiedad 
+
+        //codigo 
+        // get 
+        public int getCod()
+        {
+            return _cod;
+        }
+
+        // set 
+        public void setCod (int Cod)
+            {
+            _cod = Cod;
+        }
+        // nombre del autobus 
+        //propiedad implementada
+        public string autobusName { get; set; }
+
+      
+        /* public string getName()
+         {
+             return _autobusName;
+         }
+
+         public void setName(string Name)
+         {
+             _autobusName = Name;
+         }*/
+
+        // numero de ruta 
+        public int getNumber()
+        {
+            return _number;
+        }
+        public void setNumber(int Number)
+        {
+            _number = Number;
+        }
+
+        //cantidad de asientos disponibles 
+       public int getAmount()
+        {
+            return _amount;
+        }
+        public void serAmount( int Amount)
+        {
+            _amount = Amount;
+        }
+        
+
+      
+
+
+    }
+}
---
+++ introduccion_POO/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace introduccion_POO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // CREAMOS LA INSTANCIA DE LA CLASE AUTOBUS 
+            Autobus autobus = new Autobus();
+            
+            //creamos un objeto 
+           
+        }
+    }
+}
